250673e67a52bfa83753ed12959a5a07097c4392
Monalisa Octocat <stefania.oliviero@gmail.com>
2025-02-24 15:25:14 +0100

    1 - The original code lacked proper closing of the ResultSet and Statement objects.  This updated code uses try-with-resources to ensure that these resources are always closed, even if exceptions occur.

--- SqlInjectionLesson6a.java
+++ SqlInjectionLesson6a.java
@@ -1,11 +1,7 @@
 {
     "Componenti":  [
                        {
-                           "Snippet":  "try (ResultSet results = statement.executeQuery(query); Statement stmt = statement) {
-                                // Process the results here
-                            } catch (SQLException e) {
-                                // Handle the exception appropriately
-                            }",
+                           "Snippet":  "try (Statement statement = connection.createStatement(); ResultSet results = statement.executeQuery(query)) { \n                                // Process the results\n                            } catch (SQLException e) { \n                                //Handle the exception\n                            }",
                            "Name":  "SqlInjectionLesson6a.java"
                        }
                    ]
